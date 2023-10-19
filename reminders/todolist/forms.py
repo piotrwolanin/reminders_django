94cbb9ee174431ce0e57650c4bf8a73814480c72
piotrwolanin <peterjwolanin@gmail.com>
2023-10-19 16:14:39 +0200

    Use my own form style

--- reminders/todolist/forms.py
+++ reminders/todolist/forms.py
@@ -3,10 +3,21 @@ from django.forms import ModelForm, Textarea
 
 
 class ToDoItemModelForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove the colon from label name
+        self.label_suffix = ""
+
     class Meta:
         model = models.ToDoItem
-        fields = ["description", "priority", "completed"]
-        widgets = {"description": Textarea(attrs={"rows": 2})}
+        fields = ["description", "priority"]
+        widgets = {
+            "description": Textarea(
+                attrs={
+                    "rows": 2,
+                }
+            ),
+        }
         help_texts = {
             "description": "Maximum 200 characters.",
         }
